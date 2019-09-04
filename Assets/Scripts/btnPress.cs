fc77bfa11f23af010c02e6f68ed8aae502bf8b6d
divyaswormakai <makaidivya@gmail.com>
2019-09-04 19:18:45 +0545

    Initial changes
    -Main gameplay still to be added

---
+++ Assets/Scripts/btnPress.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btnPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static int turn = 1;
+    gameController gC;
+    void Start()
+    {
+        gC = FindObjectOfType<gameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AssignNumber(Button gameobject)
+    {
+        if (turn == 1)
+        {
+            gameobject.GetComponentInChildren<Text>().text = "X";
+            turn = 2;
+        }
+        else
+        {
+            gameobject.GetComponentInChildren<Text>().text = "O";
+            turn = 1;
+        }
+        gC.Check();
+    }
+}
---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button[] btns;
+    List<string> done;
+    List<string> undone;
+    void Start()
+    {
+        done = new List<string>();
+        undone = new List<string>();
+
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                string temp = i.ToString() + j.ToString();
+                undone.Add(temp);
+            }
+        }
+    }
+
+    public void Check()
+    {
+        
+    }
+}
